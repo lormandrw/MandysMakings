c0773fbdabe4568b99e7a95f7788c453780aed77
lormandrw <lormandrw@yahoo.com>
2015-12-02 14:49:31 -0600

    Added Rounting

--- js/controllers.js
+++ js/controllers.js
@@ -1,64 +1,76 @@
-var app = angular.module('myApp', ['ngRoute']);
+<!doctype html>
+<html lang="en"  ng-app="myApp" ng-controller="myCtrl">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>{{PageTitle}}</title>
+  
+ 	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+ 	<link rel="stylesheet" href="css/main.css" />
+  
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="bower_components/angular/angular.min.js"></script>
+	<script src="bower_components/angular-route/angular-route.min.js"></script>
+	<script src="js/products.js"></script>
+	<script src="js/controllers.js"></script>
+	<script src="js/app.js?v=1"></script>
+</head>
+<body>
 
-app.config(function($routeProvider) {
-	$routeProvider
-		// route for the home page
-		.when('/', {
-			templateUrl : 'pages/about.html',
-		})
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#about" >MM</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <div class="container">
+          <ul class="nav navbar-nav">
+			<li><a href="#about">About</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><br><div ng-paypal-cart></div></li>
+          </ul>
+        </div>
+      </div>
+  </nav>
+  
+  <div id="modalProduct" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">{{ ModelProduct.Name }}</h4>
+        </div>
+        <div class="modal-body">                                  
+          <div class="row">
+            <div class="col-xs-5">
+              <img width="200px" ng-src="images/{{ModelProduct.Image}}" alt="" /> 
+            </div>
+            <div class="col-xs-7">
+              Description: {{ ModelProduct.Description }}<br>
+              Size: {{ ModelProduct.Size }}<br>
+              Cost: {{ ModelProduct.Cost }}
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>    
+    </div>
+  </div>
+  
+  <div ng-view></div>
+  
 
-		// route for the about page
-		.when('/about', {
-			templateUrl : 'pages/about.html',
-		})
-
-		// route for the about page
-		.when('/products', {
-			templateUrl : 'pages/products.html',
-		})
-
-		// route for the contact page
-		.when('/contact', {
-			templateUrl : 'pages/contact.html',
-		})
-		.otherwise({
-			templateUrl : 'pages/about.html',
-		});
-		
-});
-
-app.controller('myCtrl', function($scope, $http) {
-	$scope.PageTitle="Mandy's Makings";
-	$scope.pageUrl="pages/products.html";
-	$scope.Products= Products;                
-	$scope.ModelProduct;
-    $scope.UpdateModalProduct = function (Product)
-    {
-		$scope.ModelProduct = Product;
-    } 
-}
-);
-
-app.directive('ngProductImage', function() {
-	return {
-		restrict: 'A',
-		template: '<img width="75px" ng-src="images/{{x.Image}}" alt="" />'
-    }
-  }
-);
-
-app.directive('ngPaypalButton', function() {
-	return {
-		restrict: 'A',
-		templateUrl: 'templates/PayPalButton.html'
-    }
-  }
-);
-
-app.directive('ngPaypalCart', function() {
-	return {
-		restrict: 'A',
-		templateUrl: 'templates/PayPalCart.html'
-    }
-  }
-);
+</body> 
+</html>
