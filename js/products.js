69b8c1e434856ed36428dd9cbbc52fe1dc656354
lormandrw <lormandrw@yahoo.com>
2015-12-06 18:50:43 -0600

    Chagned Products Page To Groups, Added New Images

--- js/products.js
+++ js/products.js
@@ -1,72 +1,62 @@
-var Products= [
+var Products = [
 	{
-	"Name":"Beard Oil - The Original",
-	"Description":"Citrus & Cedar Scent",
-	"Size":"0.5 oz",
-	"Cost":"$7.50",
-	"Image":"BeardOil.jpg"
+		"Name": "Beard Oil - Large",
+		"DescShort": "Your choice of 3 scents: 1) The Original 2) Mint Condition 3) Cabin Fever",
+		"Description": "Your choice of 3 scents: 1) The Original - Citrus & Cedar Scent 2) Mint Condition - Fresh & Minty Scent 3) Cabin Fever - Woodsy & Pine Scent",
+		"Size": "1.0 oz",
+		"Cost": "$15.00",
+		"Image": "BeardOil.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["The Original", "Mint Condition", "Cabin Fever"]
 	},
 	{
-	"Name":"Beard Oil - The Original",
-	"Description":"Citrus & Cedar Scent",
-	"Size":"1.0 oz",
-	"Cost":"$15.00",
-	"Image":"BeardOil.jpg"
+		"Name": "Beard Oil - Small",
+		"DescShort": "Your choice of 3 scents: 1) The Original 2) Mint Condition 3) Cabin Fever",
+		"Description": "Your choice of 3 scents: 1) The Original - Citrus & Cedar Scent 2) Mint Condition - Fresh & Minty Scent 3) Cabin Fever - Woodsy & Pine Scent",
+		"Size": "0.5 oz",
+		"Cost": "$7.50",
+		"Image": "BeardOil.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["The Original", "Mint Condition", "Cabin Fever"]
 	},
 	{
-	"Name":"Beard Oil - Mint Condition",
-	"Description":"Fresh & Minty Scent",
-	"Size":"0.5 oz",
-	"Cost":"$7.50",
-	"Image":"BeardOil.jpg"
+		"Name": "Bath Salts",
+		"DescShort": "Your choice of 4 scents: 1) Relaxing Lavender 2) Rejuvenation 3) Restore 4) Refresh",
+		"Description": "Your choice of 4 scents: 1) Relaxing Lavender - Perfect for rest and relaxation, 2) Rejuvenation - An energizing mint blend 3) Restore - A blend of soothing oils ideal for tired, overworked muscles 4) Refresh - A fresh citrus blend",
+		"Size": "4.0 oz",
+		"Cost": "$4.00",
+		"Image": "BathSalts.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["Relaxing Lavender", "Rejuvenation", "Restore", "Refresh"]
 	},
 	{
-	"Name":"Beard Oil - Mint Condition",
-	"Description":"Fresh & Minty Scent",
-	"Size":"1.0 oz",
-	"Cost":"$15.00",
-	"Image":"BeardOil.jpg"
+		"Name": "Lip Balm",
+		"DescShort": "Your choice of 2 scents: 1) Peppermint 2) Unscented",
+		"Description": "Your choice of 2 scents: 1) Peppermint 2) Unscented",
+		"Size": "1 ct",
+		"Cost": "$2.00",
+		"Image": "LipBalm.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["Peppermint", "Unscented"]
 	},
 	{
-	"Name":"Beard Oil - Cabin Fever",
-	"Description":"Woodsy & Pine Scent",
-	"Size":"0.5 oz",
-	"Cost":"$7.50",
-	"Image":"BeardOil.jpg"
+		"Name": "Lady Soap",
+		"DescShort": "Your choice of 4 scents: 1) Lavendar 2) Mint 3) Citrus 4) Lemongrass",
+		"Description": "Your choice of 4 scents: 1) Lavendar 2) Mint 3) Citrus 4) Lemongrass",
+		"Size": "1 ct",
+		"Cost": "$4.00",
+		"Image": "LadySoap.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["Lavendar", "Mint", "Citrus", "Lemongrass"]
 	},
 	{
-	"Name":"Beard Oil - Cabin Fever",
-	"Description":"Woodsy & Pine Scent",
-	"Size":"1.0 oz",
-	"Cost":"$15.00",
-	"Image":"BeardOil.jpg"
-	},
-	{
-	"Name":"Bath Salts - R&R",
-	"Description":"Relaxing lavender perfect for rest and relaxa",
-	"Size":"4.0 oz",
-	"Cost":"$4.00",
-	"Image":"BathSalts.jpg"
-	},
-	{
-	"Name":"Bath Salts - Rejuvenation",
-	"Description":"An energizing mint blend",
-	"Size":"4.0 oz",
-	"Cost":"$4.00",
-	"Image":"BathSalts.jpg"
-	},
-	{
-	"Name":"Bath Salts - Restore",
-	"Description":"A blend of soothing oils ideal for tired, overworked muscles",
-	"Size":"4.0 oz",
-	"Cost":"$4.00",
-	"Image":"BathSalts.jpg"
-	},
-	{
-	"Name":"Bath Salts - Refresh",
-	"Description":"A fresh citrus blend",
-	"Size":"4.0 oz",
-	"Cost":"$4.00",
-	"Image":"BathSalts.jpg"
+		"Name": "Manly Soap",
+		"DescShort": "Your choice of 3 scents: 1) The Original 2) Mint Condition 3) Cabin Fever",
+		"Description": "Your choice of 3 scents: 1) The Original - Citrus & Cedar Scent 2) Mint Condition - Fresh & Minty Scent 3) Cabin Fever - Woodsy & Pine Scent",
+		"Size": "1 ct",
+		"Cost": "$4.00",
+		"Image": "ManlySoap.jpg",
+		"hosted_button_id": "9XD3GBGFY6B56",
+		"Scent": ["The Original", "Mint Condition", "Cabin Fever"]
 	}
 ]
