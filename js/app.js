a1ef2fd17b7b5a7a6c79a4ac457090fff7955b16
lormandrw <lormandrw@yahoo.com>
2015-12-12 08:18:38 -0600

    Removed Everything Not Needed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-function getContinueShoppingURL(form) {
-	// -- Get the href of the currently displayed webpage --
-	form.shopping_url.value = window.location.href;
-}
-
 $(document).on('click', '.navbar-collapse.in', function (e) {
     if ($(e.target).is('a')) {
         $(this).collapse('hide');
